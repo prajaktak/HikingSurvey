4d4b9b894ae657cd5fafa3d99c2b3a867e1b95e5
Prajakta Kulkarni <your.email@example.com>
2025-10-06 20:47:12 +0200

    Initial Commit

---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Prajakta Kulkarni on 06/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HikingSurveyTests/HikingSurveyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyTests.swift
+//  HikingSurveyTests
+//
+//  Created by Prajakta Kulkarni on 06/10/2025.
+//
+
+import Testing
+@testable import HikingSurvey
+
+struct HikingSurveyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
